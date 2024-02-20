a809250c49db60370427cf5ae76b7a9d19745129
alekarhis <alekarhis@pm.me>
2024-02-20 18:57:20 +0100

    6.5 - sort by votes

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -29,17 +29,20 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      {anecdotes
+        .sort((a, b) => a.votes < b.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+        )
+      }
       <h2>create new</h2>
       <form onSubmit={handleCreate}>
         <div><input name="anecdote" /></div>
