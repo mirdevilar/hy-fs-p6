92cc2eeb5053a6cb93ec19572fb95ca7f6f1c965
alekarhis <alekarhis@pm.me>
2024-02-21 23:00:37 +0100

    6.14 - implement backend json-server with initial data initialization

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 - Pure functions are such, that they do not cause any side effects and they must always return the same response when called with the same parameters.
 - npm install --save-dev jest @babel/preset-env @babel/preset-react eslint-plugin-jest
 
+#6b. Redux toolkit:
+- install library @reduxjs/toolkit
+- configureStore()
+- investigate error: "Selector unknown returned a different result when called with the same parameters. This can lead to unnecessary rerenders.
+Selectors that return a new reference (such as an object or an array) should be memoized: https://redux.js.org/usage/deriving-data-selectors#optimizing-selectors-with-memoization "
+
+
--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,9 +1,22 @@
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { actionSetAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes.js'
+
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => {
+      dispatch(actionSetAnecdotes(anecdotes))
+    })
+  }, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
