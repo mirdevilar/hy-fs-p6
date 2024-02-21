23850ada2d0aa99f83bf21af1bd612e55df6bee5
alekarhis <alekarhis@pm.me>
2024-02-21 13:30:37 +0100

    move store setup to store.js using redux toolkit configureStore

---
+++ redux-anecdotes/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+  }
+})
+
+export default store
